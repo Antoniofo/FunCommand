d3456cebb9d3fdf8cff8e2ab26ceb76563441822
Antoniofo <antonio57201@gmail.com>
2021-12-28 19:52:15 +0100

    correcting files

---
+++ FunCommand/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using Synapse.Api.Plugin;
+
+namespace FunCommand
+{
+    [PluginInformation(
+        Author = "Antoio",
+        Description ="Fun command to play with",
+        LoadPriority =0,
+        Name = "FunCommand",
+        SynapseMajor =2,
+        SynapseMinor =8,
+        SynapsePatch =0,
+        Version = "v1.0.1"
+        )]
+    public class Plugin : AbstractPlugin
+    {
+        [Synapse.Api.Plugin.Config(section = "FunCommand")]
+        public static Config Config { get; set; }
+        public override void Load()
+        {
+            base.Load();
+        }
+
+        public override void ReloadConfigs()
+        {
+            base.ReloadConfigs();
+        }
+    }
+}
